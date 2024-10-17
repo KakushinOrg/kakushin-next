798478899b95bb7b20c74b0a6e9b7fcf9029fe0d
Beaowulf <68222371+Beaowulf@users.noreply.github.com>
2024-10-17 21:10:20 +0300

    Update OurProjects.js

--- src/app/landing_page/OurProjects.js
+++ src/app/landing_page/OurProjects.js
@@ -33,34 +33,34 @@ const CustomPrevArrow = ({ onClick }) => {
 };
 
 let projects = [
-  { title: "Debate", image: "/images/projects/debate.png" },
-  { title: "L3arn", image: "/images/projects/l3arn.png" },
-  { title: "Kakushin Blog", image: "/images/projects/kakushinBlog.png" },
+  { title: "Debate", src: "/images/projects/debate.png" },
+  { title: "L3arn", src: "/images/projects/l3arn.png" },
+  { title: "Kakushin Blog", src: "/images/projects/kakushinBlog.png" },
   {
     title: "Kakushin Postcard",
     image: "/images/projects/kakushinPostCard.png",
   },
-  { title: "Kinex AI", image: "/images/projects/kinexaiApp.png" },
-  { title: "Kinex AI", image: "/images/projects/kinexAIApp2.png" },
+  { title: "Kinex AI", src: "/images/projects/kinexaiApp.png" },
+  { title: "Kinex AI", src: "/images/projects/kinexAIApp2.png" },
   {
     title: "Kinex AI Illustration",
     image: "/images/projects/kinexAIIllustration.png",
   },
-  { title: "My BNB", image: "/images/projects/myBnb.png" },
-  { title: "My Sizer", image: "/images/projects/mySizer.png" },
-  { title: "My Sizer App", image: "/images/projects/mysizerApp.png" },
-  { title: "Riddl3", image: "/images/projects/riddl3.png" },
-  { title: "Urban", image: "/images/projects/urban.png" },
-  { title: "Urbane", image: "/images/projects/urbane.png" },
-  { title: "RAID", image: "/images/projects/raid.png" },
-  { title: "Kakushin Illustration", image: "/images/projects/P2.png" },
-  { title: "Kakushin Illustration", image: "/images/projects/P7.png" },
-  { title: "Kakushin Illustration", image: "/images/projects/P9.png" },
-  { title: "Kakushin Illustration", image: "/images/projects/P10.png" },
-  { title: "Kakushin Illustration", image: "/images/projects/P17.png" },
-  { title: "Kakushin Illustration", image: "/images/projects/P19.png" },
-  { title: "Kakushin Illustration", image: "/images/projects/P21.png" },
-  { title: "Kakushin Illustration", image: "/images/projects/P22.png" },
+  { title: "My BNB", src: "/images/projects/myBnb.png" },
+  { title: "My Sizer", src: "/images/projects/mySizer.png" },
+  { title: "My Sizer App", src: "/images/projects/mysizerApp.png" },
+  { title: "Riddl3", src: "/images/projects/riddl3.png" },
+  { title: "Urban", src: "/images/projects/urban.png" },
+  { title: "Urbane", src: "/images/projects/urbane.png" },
+  { title: "RAID", src: "/images/projects/raid.png" },
+  { title: "Kakushin Illustration", src: "/images/projects/P2.png" },
+  { title: "Kakushin Illustration", src: "/images/projects/P7.png" },
+  { title: "Kakushin Illustration", src: "/images/projects/P9.png" },
+  { title: "Kakushin Illustration", src: "/images/projects/P10.png" },
+  { title: "Kakushin Illustration", src: "/images/projects/P17.png" },
+  { title: "Kakushin Illustration", src: "/images/projects/P19.png" },
+  { title: "Kakushin Illustration", src: "/images/projects/P21.png" },
+  { title: "Kakushin Illustration", src: "/images/projects/P22.png" },
 ];
 
 const OurProjects = () => {
@@ -107,8 +107,10 @@ const OurProjects = () => {
           <div key={index} className="text-center">
             <div className="relative flex flex-col items-center justify-between bg-white shadow-md rounded-lg h-[380px] w-[300px] mx-auto my-10 border-2 border-gray-100">
               <div className="relative w-full h-[240px] overflow-hidden rounded-t-lg">
-                <img
-                  src={project.image}
+                <Image
+                  width={430}
+                  height={430}
+                  src={project.src}
                   alt={project.title}
                   className="object-contain w-full h-full"
                 />
