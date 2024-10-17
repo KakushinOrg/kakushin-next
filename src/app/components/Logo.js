d43650b474d080d8ba12f0ae6bfa0e58021d90f6
Beaowulf <68222371+Beaowulf@users.noreply.github.com>
2024-10-17 18:19:54 +0300

    Update Logo.js

--- src/app/components/Logo.js
+++ src/app/components/Logo.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import WhiteLogo from "../../../public/images/logo-footer-white.png";
-import BlackLogo from "../../../public/images/logo-footer-black.png";
+import WhiteLogo from "/images/logo-footer-white.png";
+import BlackLogo from "/images/logo-footer-black.png";
 
 const Logo = ({ color, className }) => {
   return (
